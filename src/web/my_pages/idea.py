2e60f392760d556eee6399468f1a795ab0311879
hibana2077 <hibana2077@gmail.com>
2024-07-14 15:25:27 +0800

    chore: Add login functionality to Idea and Settings pages

--- src/web/my_pages/idea.py
+++ src/web/my_pages/idea.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title('Idea')
+
+if "login" not in st.session_state:
+    st.session_state.login = False
+
+if st.session_state.login:
+    st.write('Idea')
+else:
+    st.write('Please login first')
--- src/web/my_pages/settings.py
+++ src/web/my_pages/settings.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title('Settings')
+
+if "login" not in st.session_state:
+    st.session_state.login = False
+
+if st.session_state.login:
+    st.write('Settings')
+else:
+    st.write('Please login first')

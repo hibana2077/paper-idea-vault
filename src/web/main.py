f6d0e7819dff58541599c5700f776f10daf526ed
hibana2077 <hibana2077@gmail.com>
2024-07-14 15:23:04 +0800

    chore: Update Dockerfile and add main.py for web app

--- src/web/main.py
+++ src/web/main.py
@@ -1,4 +1,9 @@
+import time
+import os
 import streamlit as st
+
+USER = os.getenv('USER', 'user')
+PASSWORD = os.getenv('PASSWORD', 'password')
 
 pg = st.navigation(
     [
@@ -7,4 +12,20 @@ pg = st.navigation(
     ]
 )
 
-pg.run()
+if "login" not in st.session_state:
+    st.session_state.login = False
+
+if st.session_state.login:
+    pg.run()
+else:
+    with st.form(key='login'):
+        st.text_input('Username')
+        st.text_input('Password', type='password')
+        if st.form_submit_button('Login'):
+            if st.session_state.username == USER and st.session_state.password == PASSWORD:
+                st.success('Login successful')
+                st.session_state.login = True
+                time.sleep(1.2)
+                pg.run()
+            else:
+                st.error('Invalid username or password')

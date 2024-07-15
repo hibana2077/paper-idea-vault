e0b748043bcd8155028ac6cb131b6ed39026899a
hibana2077 <hibana2077@gmail.com>
2024-07-15 10:36:55 +0800

    chore: Update web app to include new idea form

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,11 +1,3 @@
-'''
-Author: hibana2077 hibana2077@gmail.com
-Date: 2024-05-06 21:09:40
-LastEditors: hibana2077 hibana2077@gmaill.com
-LastEditTime: 2024-06-13 15:49:11
-FilePath: \Digital-TA\src\backend\main.py
-Description: Here is the main file for the FastAPI server.
-'''
 from langchain_community.vectorstores.faiss import FAISS
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.embeddings import OllamaEmbeddings
--- src/web/my_pages/idea.py
+++ src/web/my_pages/idea.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import requests
+import time
+import os
+
+BACKEND_URL = os.getenv('BACKEND_URL', 'http://localhost:8081')
+
+@st.experimental_dialog('New Idea')
+def new_idea():
+    with st.form(key='new_idea'):
+        st.text_input('Title', key='title')
+        st.text_area('Description', key='description')
+        # Need to add more fields here and submit to backend
+        
 
 st.title('Idea')
 
@@ -7,5 +20,6 @@ if "login" not in st.session_state:
 
 if st.session_state.login:
     st.write('Idea')
+    idea_left_col, idea_right_col = st.columns([7,3],vertical_alignment='center')
 else:
     st.write('Please login first')
